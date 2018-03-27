9ab0701f8da4e7eef2a24a12bc1251428bcf1e22
erika21404 <405410480@s05.tku.edu.tw>
2018-03-27 21:54:06 +0800

    3/27

--- app/src/main/java/eisc/tku/dont/MainActivity.java
+++ app/src/main/java/eisc/tku/dont/MainActivity.java
@@ -1,11 +1,13 @@
 package eisc.tku.dont;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,12 +35,31 @@ public class MainActivity extends AppCompatActivity {
         send = findViewById(R.id.button);
         printout = findViewById(R.id.textView);
     }
+    public void printresult2(View v){
+        String i=id.getText().toString();
+        String n=name.getText().toString();
+        String e=email.getText().toString();
+        //printout.setTextSize(size);
+        //printout.setText(i+n+e);
+        Toast.makeText(this,"學號"+i+"\n"+n+" 您好"+"\n"+"您的email是: "+e+"\n",Toast.LENGTH_LONG).show();
+    }
     public void printresult(View v){
         String i=id.getText().toString();
         String n=name.getText().toString();
         String e=email.getText().toString();
-        printout.setTextSize(size);
-        printout.setText(i+n+e);
+        //printout.setTextSize(size);
+        //printout.setText(i+n+e);
+       // Toast.makeText(this,"學號"+i+"\n"+n+" 您好"+"\n"+"您的email是: "+e+"\n",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setMessage("學號"+i+"\n"+n+" 您好"+"\n"+"您的email是: "+e+"\n");
+        builder.setTitle("print");
+        builder.setPositiveButton("ok",null);
+        builder.setNeutralButton("Nothing",null);
+        builder.setNegativeButton("cancel",null);
+        builder.show();
+
+
+
     }
 
 }
